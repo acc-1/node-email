f2f49db52f585bb2da1c2505137a2aa3970b9ebe
Agus <agusalt2004@hotmail.com>
2024-03-24 08:15:11 -0300

    m

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json({
 }))
 
 app.use(cors({
-    origin: 'https://inscripciones-club-ciclon.netlify.app/home'
+    origin: 'https://inscripciones-club-ciclon.netlify.app'
 }));
 // #endregion
 
