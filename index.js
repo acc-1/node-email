32caf1675adbebaea1ea7f68a9b4f63324e0c736
Agus <agusalt2004@hotmail.com>
2024-03-27 02:03:24 -0300

    m

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = 3000
 
 let datosArr = [];
 const salidaXLSXPath = './salida.xlsx';
-
+let salidaXLSXContent;
 let datosCompletos = [];
 // #region
 async function main() {
-    const salidaXLSXContent = fs.readFileSync(salidaXLSXPath);
+    salidaXLSXContent = fs.readFileSync(salidaXLSXPath);
     const workbook = await XlsxPopulate.fromBlankAsync();
     workbook.sheet(0).cell('A1').value('NOMBRE');
     workbook.sheet(0).cell('B1').value('APELLIDO');
