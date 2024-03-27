2cd2edfaa01b9a919bffb5f891b88d374fbd5942
Agus <agusalt2004@hotmail.com>
2024-03-27 02:26:23 -0300

    m

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import XlsxPopulate from 'xlsx-populate';
 import fs from 'fs';
-const app=express()
-const port = 3000
+import os from 'os'; // Importa el módulo 'os'
+
+const app = express();
+const port = 3000;
 
 let datosArr = [];
-const salidaXLSXPath = './salida.xlsx';
+const salidaXLSXPath = `${os.tmpdir()}/salida.xlsx`; // Usa el directorio temporal del sistema
 let datosCompletos = [];
 let salidaXLSXContent;
-// #region
+
 async function main() {
     const workbook = await XlsxPopulate.fromBlankAsync();
     workbook.sheet(0).cell('A1').value('NOMBRE');
@@ -45,10 +47,9 @@ async function main() {
     salidaXLSXContent = fs.readFileSync(salidaXLSXPath);
 }
 
-
 app.use(
     express.urlencoded({
-        extended:true
+        extended: true
     })
 )
 
@@ -57,69 +58,60 @@ app.use(express.json({
 }))
 
 app.use(cors({
-  origin: ['https://inscripciones-club-ciclon.netlify.app', 'https://inscripciones-club-ciclon.netlify.app/home']
+    origin: ['https://inscripciones-club-ciclon.netlify.app', 'https://inscripciones-club-ciclon.netlify.app/home']
 }));
-// #endregion
 
-
-
-
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('funciona correctamente');
 })
 
 app.post('/datos', async (req, res) => {
-  let datos = req.body;
-  datosArr = [datos];
-  datosCompletos.push(datos)
-  await main();
-  const resend = new Resend('re_BESoasix_834sJRhhpnnofMrFQ1WqVWHR');
+    let datos = req.body;
+    datosArr = [datos];
+    datosCompletos.push(datos)
+    await main();
+    const resend = new Resend('re_BESoasix_834sJRhhpnnofMrFQ1WqVWHR');
 
-  try {
-    
-      const { data, error } = await resend.emails.send({
-          from: 'Pre-Inscripciones Club Ciclon<onboarding@resend.dev>',
-          to: ['agusalt2004@hotmail.com'],
-          subject: `${datosArr[0].nombre} NUEVA PRE-INSCRIPCION`,
-          html: `<p>
-          <strong>NOMBRE:</strong> ${datosArr[0].nombre}<br><br>
-          <strong>APELLIDO:</strong> ${datosArr[0].apellido}<br><br>
-          <strong>SEXO:</strong> ${datosArr[0].sexo}<br><br>
-          <strong>FECHA DE NACIMIENTO:</strong> ${datosArr[0].fecha_nacimiento}<br><br>
-          <strong>DOCUMENTO:</strong> ${datosArr[0].documento}<br><br>
-          <strong>CIUDAD:</strong> ${datosArr[0].ciudad}<br><br>
-          <strong>DOMICILIO:</strong> ${datosArr[0].domicilio}<br><br>
-          <strong>EDAD:</strong> ${datosArr[0].edad}<br><br>
-          <strong>TIPO DE CARRERA:</strong> ${datosArr[0].carrera}<br><br>
-          <strong>TELEFONO:</strong> ${datosArr[0].telefono}<br><br>
-          <strong>EMAIL:</strong> ${datosArr[0].email}<br><br>
-          </p>`,
-          attachments: [
-            {
-              filename: 'pre-inscripciones-Club-Ciclon.xlsx',
-              content: salidaXLSXContent,
-            },
-          ],
-      });
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Pre-Inscripciones Club Ciclon<onboarding@resend.dev>',
+            to: ['agusalt2004@hotmail.com'],
+            subject: `${datosArr[0].nombre} NUEVA PRE-INSCRIPCION`,
+            html: `<p>
+            <strong>NOMBRE:</strong> ${datosArr[0].nombre}<br><br>
+            <strong>APELLIDO:</strong> ${datosArr[0].apellido}<br><br>
+            <strong>SEXO:</strong> ${datosArr[0].sexo}<br><br>
+            <strong>FECHA DE NACIMIENTO:</strong> ${datosArr[0].fecha_nacimiento}<br><br>
+            <strong>DOCUMENTO:</strong> ${datosArr[0].documento}<br><br>
+            <strong>CIUDAD:</strong> ${datosArr[0].ciudad}<br><br>
+            <strong>DOMICILIO:</strong> ${datosArr[0].domicilio}<br><br>
+            <strong>EDAD:</strong> ${datosArr[0].edad}<br><br>
+            <strong>TIPO DE CARRERA:</strong> ${datosArr[0].carrera}<br><br>
+            <strong>TELEFONO:</strong> ${datosArr[0].telefono}<br><br>
+            <strong>EMAIL:</strong> ${datosArr[0].email}<br><br>
+            </p>`,
+            attachments: [
+                {
+                    filename: 'pre-inscripciones-Club-Ciclon.xlsx',
+                    content: salidaXLSXContent,
+                },
+            ],
+        });
 
-      if (error) {
-          console.error({ error });
-          res.status(500).send('Error  al enviar el correo electrónico.');
-      } else {
-        await main();
-          console.log('Correo electrónico enviado correctamente:', data);
-          res.send(); // Envía una respuesta vacía
-      }
-  } catch (err) {
-      console.error('Error al enviar el correo electrónico:', err);
-      res.status(500).send('Error al enviar el correo electrónico.');
-  }
+        if (error) {
+            console.error({ error });
+            res.status(500).send('Error  al enviar el correo electrónico.');
+        } else {
+            await main();
+            console.log('Correo electrónico enviado correctamente:', data);
+            res.send(); // Envía una respuesta vacía
+        }
+    } catch (err) {
+        console.error('Error al enviar el correo electrónico:', err);
+        res.status(500).send('Error al enviar el correo electrónico.');
+    }
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`estoy ejecutandome en http://localhost:${port}`)
 })
-
-
-
-
